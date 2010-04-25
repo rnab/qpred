765e4a56020d34a965c22090d6e33d07827d07bc
s.d.hippisley-cox <s.d.hippisley-cox@4c1e71fb-02a8-40ad-a7d9-f2bb831ec887>
2010-04-25 22:16:35 +0000

    Initial release.

---
+++ qrisk2/include/irisk/Q54_general_xml_74_cvd_0.h
@@ -0,0 +1,33 @@
+/* 
+ * Copyright 2010 ClinRisk Ltd.
+ * 
+ * This file is part of QRISK2-2010 (http://qrisk.org).
+ * 
+ * QRISK2-2010 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * QRISK2-2010 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with QRISK2-2010.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * The initial version of this file, to be found at http://qrisk.org, faithfully implements QRISK2-2010.
+ * We have released this code under the GNU Lesser General Public License to enable others to implement the algorithm faithfully.
+ * However, the nature of the GNU Lesser General Public License is such that we cannot prevent, for example, someone randomly permuting the coefficients.
+ * We stress, therefore, that it is the responsibility of the end user to check that the source that they receive produces the same results as the original code posted at http://qrisk.org.
+ * Inaccurate implementations of risk scores can lead to wrong patients being given the wrong treatment.
+ *
+ * This file has been auto-generated.
+ * XML source: Q54_general_xml_74_cvd_0.xml
+ * STATA dta time stamp: 26 Feb 2010 13:37
+ * Header file create date: Fri Apr 16 11:10:40 BST 2010
+ */
+
+double cvd_female(
+int age,int b_AF,int b_ra,int b_renal,int b_treatedhyp,int b_type2,double bmi,int ethrisk,int fh_cvd,double rati,double sbp,int smok,int surv,double town,int *error,char *errorBuf,int errorBufSize
+);
