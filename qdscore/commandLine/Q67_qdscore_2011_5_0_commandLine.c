4784cbbd367cff19c3c10f344b6487455a4721c7
s.d.hippisley-cox <s.d.hippisley-cox@4c1e71fb-02a8-40ad-a7d9-f2bb831ec887>
2011-06-22 11:21:30 +0000

    Missing files.

---
+++ qdscore/commandLine/Q67_qdscore_2011_5_0_commandLine.c
@@ -0,0 +1,95 @@
+/* 
+ * Copyright 2011 ClinRisk Ltd.
+ * 
+ * This file is part of QDScore (http://qdscore.org, http://svn.clinrisk.co.uk/opensource/qdscore).
+ * 
+ * QDScore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * QDScore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with QDScore.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * The initial version of this file, to be found at http://svn.clinrisk.co.uk/opensource/qdscore, faithfully implements QDScore.
+ * We have released this code under the GNU Lesser General Public License to enable others to implement the algorithm faithfully.
+ * However, the nature of the GNU Lesser General Public License is such that we cannot prevent, for example, someone altering the coefficients.
+ * We stress, therefore, that it is the responsibility of the end user to check that the source that they receive produces the same results as the original code posted at http://svn.clinrisk.co.uk/opensource/qdscore.
+ * Inaccurate implementations of risk scores can lead to wrong patients being given the wrong treatment.
+ * 
+ * This file has been auto-generated.
+ * XML source: Q67_qdscore_2011_5_0.xml
+ * STATA dta time stamp: 9 May 2011 11:01
+ * C file create date: Wed 22 Jun 2011 10:48:53 BST
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <clinrisk/Q67_qdscore_2011_5_0.h>
+
+static double score[16];
+static char errorBuf[1024];
+static int error;
+
+void usage(void) {
+	printf(" * Copyright 2011 ClinRisk Ltd.\n");
+	printf(" * \n");
+	printf(" * This is part of QDScore (http://qdscore.org, http://svn.clinrisk.co.uk/opensource/qdscore).\n");
+	printf(" * \n");
+	printf(" * QDScore is free software: you can redistribute it and/or modify\n");
+	printf(" * it under the terms of the GNU Lesser General Public License as published by\n");
+	printf(" * the Free Software Foundation, either version 3 of the License, or\n");
+	printf(" * (at your option) any later version.\n");
+	printf(" * \n");
+	printf(" * QDScore is distributed in the hope that it will be useful,\n");
+	printf(" * but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+	printf(" * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n");
+	printf(" * GNU Lesser General Public License for more details.\n");
+	printf(" * \n");
+	printf(" * You should have received a copy of the GNU Lesser General Public License\n");
+	printf(" * along with QDScore.  If not, see <http://www.gnu.org/licenses/>.\n");
+	printf(" * \n");
+	printf(" * The initial version of this file, to be found at http://svn.clinrisk.co.uk/opensource/qdscore, faithfully implements QDScore.\n");
+	printf(" * We have released this code under the GNU Lesser General Public License to enable others to implement the algorithm faithfully.\n");
+	printf(" * However, the nature of the GNU Lesser General Public License is such that we cannot prevent, for example, someone altering the coefficients.\n");
+	printf(" * We stress, therefore, that it is the responsibility of the end user to check that the source that they receive produces the same results as the original code posted at http://svn.clinrisk.co.uk/opensource/qdscore.\n");
+	printf(" * Inaccurate implementations of risk scores can lead to wrong patients being given the wrong treatment.\n");
+	printf(" *\n");
+	printf(" * This file has been auto-generated.\n");
+	printf(" * XML source: Q67_qdscore_2011_5_0.xml\n");
+	printf(" * STATA dta time stamp: 9 May 2011 11:01\n");
+	printf(" * C file create date: Wed 22 Jun 2011 10:48:53 BST\n");
+	printf(" *\n");
+	printf("Usage:\n");
+	printf("  Q67_qdscore_2011_5_0_commandLine age b_corticosteroids b_cvd b_treatedhyp bmi ethrisk fh_diab smoke_cat surv town\n");
+}
+
+int main (int argc, char *argv[]) {
+	if (argc!=11) {
+		usage();
+		exit(1);
+	}
+
+	int age = atoi(argv[1]);
+	int b_corticosteroids = atoi(argv[2]);
+	int b_cvd = atoi(argv[3]);
+	int b_treatedhyp = atoi(argv[4]);
+	double bmi = atof(argv[5]);
+	int ethrisk = atoi(argv[6]);
+	int fh_diab = atoi(argv[7]);
+	int smoke_cat = atoi(argv[8]);
+	int surv = atoi(argv[9]);
+	double town = atof(argv[10]);
+
+	double score = type2_female(age,b_corticosteroids,b_cvd,b_treatedhyp,bmi,ethrisk,fh_diab,smoke_cat,surv,town,&error,errorBuf,sizeof(errorBuf));
+	if (error) {
+		printf("%s", errorBuf);
+		exit(1);
+	}
+	printf("%f\n", score);
+}
